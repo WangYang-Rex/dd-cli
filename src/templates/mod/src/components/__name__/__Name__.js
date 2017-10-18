6d8e67952e9750be8e6ebd56f7ce4c81da22e936
wangyang <617418875@qq.com>
2017-10-18 15:12:07 +0800

    add

--- src/templates/mod/src/components/__name__/__Name__.js
+++ src/templates/mod/src/components/__name__/__Name__.js
@@ -3,27 +3,27 @@ import './<%- Name %>.styl';
 
 class <%- Name %> extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-		};
-	}
+constructor(props) {
+	super(props);
+	this.state = {
+	};
+}
 
-	render() {
-		return (
-			<div className="<%= name %>">
-				component <%= name %>
-			</div>
-		);
-	}
-    
-	componentWillMount() {}
-	componentDidMount() {}
-	componentWillReceiveProps(nextProps) {}
-	shouldComponentUpdate(nextProps, nextState) { return true; }
-	componentWillUpdate(nextProps, nextState) {}
-	componentDidUpdate(prevProps, prevState) {}
-	componentWillUnmount() {}
+render() {
+  return (
+		<div className="<%= name %>">
+			component <%= name %>
+		</div>
+	);
+}
+	
+componentWillMount() {}
+componentDidMount() {}
+componentWillReceiveProps(nextProps) {}
+shouldComponentUpdate(nextProps, nextState) { return true; }
+componentWillUpdate(nextProps, nextState) {}
+componentDidUpdate(prevProps, prevState) {}
+componentWillUnmount() {}
 }
 
 export default <%- Name %>;
